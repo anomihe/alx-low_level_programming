a4870f2042721eb6fdca57566f43295df0d79c35
anomihe <anomihekenneth@gmail.com>
2022-06-03 13:57:48 -0700

    first c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
@@ -16,17 +17,26 @@ int main(void)
 	
 	if ( n > 0 )
 	{
+
 	printf("%d is positive\n", n);
+	
 	}
 
 	else if ( n == 0 )
+	
 	{
+	
 	printf("%d is zero\n", n);
+	
 	}
 
 	else
+	
 	{ 
+	
 	printf("%d is negative\n", n);
+	
 	}
+	
 	return (0);
 }
