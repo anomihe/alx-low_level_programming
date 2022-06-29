bff5c637062b1053ed5c4c3e94f4d79e54d48ddb
anomihe <anomihekenneth@gmail.com>
2022-06-29 07:13:19 -0700

    memset

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,18 +1,22 @@
 #include "main.h"
-
 /**
- * _memset - memset function
- * @s: start point of string to change
- * @b: value that will replace
- * @n: number of bytes to change
- * Return: changed pointer
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
  */
 
 char *_memset(char *s, char b, unsigned int n)
-{
-	char *p = s;
 
-	for (; n != 0; p++, n--)
-		*p = b;
+{
+	unsigned int i;
+
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
+	} /*END FOR*/
+
 	return (s);
 }
